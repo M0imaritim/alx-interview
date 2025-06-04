46cc32dadc3b951a14a8423a8b719ca8dab4db36
Moimaritim <moimaritim@gmail.com>
2025-06-04 20:28:28 +0300

    pystyle fix

--- 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
+++ 0x07-rotate_2d_matrix/0-rotate_2d_matrix.py
@@ -3,10 +3,11 @@
 This module provides a function to rotate a 2D matrix 90 degrees clockwise.
 """
 
+
 def rotate_2d_matrix(matrix):
     """
     Rotates a given n x n 2D matrix 90 degrees clockwise in-place.
-    
+
     Args:
         matrix (list of lists): The matrix to rotate.
     """
