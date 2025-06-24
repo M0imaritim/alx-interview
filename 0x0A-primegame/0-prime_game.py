85f0b98466a7035fb7977dfcb211c17ee2a1548a
Moimaritim <moimaritim@gmail.com>
2025-06-24 19:35:51 +0300

    Prime game

---
+++ 0x0A-primegame/0-prime_game.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+
+def isWinner(x, nums):
+    if not nums or x < 1:
+        return None
+
+    n = max(nums)
+
+    # Sieve of Eratosthenes
+    is_prime = [False, False] + [True for _ in range(2, n + 1)]
+    for i in range(2, int(n**0.5) + 1):
+        if is_prime[i]:
+            for j in range(i*i, n+1, i):
+                is_prime[j] = False
+
+    # Precompute number of primes up to each index
+    prime_count = [0] * (n + 1)
+    count = 0
+    for i in range(len(prime_count)):
+        if is_prime[i]:
+            count += 1
+        prime_count[i] = count
+
+    maria_wins = 0
+    ben_wins = 0
+
+    for num in nums:
+        if prime_count[num] % 2 == 1:
+            maria_wins += 1
+        else:
+            ben_wins += 1
+
+    if maria_wins > ben_wins:
+        return "Maria"
+    elif ben_wins > maria_wins:
+        return "Ben"
+    else:
+        return None
---
+++ 0x0A-primegame/README.md
@@ -0,0 +1,7 @@
+# Prime Game
+
+This project involves solving a game theory problem where two players take turns selecting prime numbers and removing them and their multiples from a set of numbers.
+
+## Task
+
+Determine the winner of each game based on optimal strategy.
